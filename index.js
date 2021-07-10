36f836c7a97e6370d2b7d3213ac4725ad66198a5
David Dreggors <ddreggors@redhat.com>
2021-07-09 22:46:04 -0400

    Adding a bit of HTML formatting for fun

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const http = require('http');
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello S2I World');
+  var html = buildHtml(req);
+  res.writeHead(200, {
+    'Content-Type': 'text/html',
+    'Content-Length': html.length,
+    'Expires': new Date().toUTCString()
+  });
+  res.end(html);
 });
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+function buildHtml(req) {
+  var header = '';
+  var body = '<h1>Hello S2I World</h1>\n<br/><hr>\n<br/>\n<p>Thanks for stopping by!</p>';
+
+  // concatenate header string
+  // concatenate body string
+
+  return '<!DOCTYPE html>'
+       + '<html><head>' + header + '</head><body>' + body + '</body></html>';
+};
